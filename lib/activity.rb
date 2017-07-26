ddb63567031b5c91e1bb2cd6db742c0cadc0c95f
Nate <nategiesing@gmail.com>
2017-07-26 10:00:53 -0600

    almost got cost working for both participants

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,30 @@
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(key, value)
+    participants[key] = value
+    return participants
+
+  end
+
+  def total_cost(key, value)
+    value
+    # sum = []
+    # # require "pry"; binding.pry
+    # participants.each do |value|
+    #   sum << value
+    # end
+    # sum.reduce(:+)
+  end
+
+  def split
+    total_cost / participants.count
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,56 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new("Brunch")
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_knows_name
+    activity = Activity.new("Brunch")
+    expected = "Brunch"
+
+    assert_equal expected, activity.name
+  end
+
+  def test_participants
+    activity = Activity.new("Brunch")
+    expected = {}
+
+    assert_equal expected, activity.participants
+  end
+
+  def test_it_adds_participant
+    activity = Activity.new("Brunch")
+    expected = activity.participants
+
+    assert_equal expected, activity.add_participant("Jim", 20)
+  end
+
+  def test_it_adds_another_participant
+    # skip
+    activity = Activity.new("Brunch")
+    expected = {"Joe" => 40}
+
+    assert_equal expected, activity.add_participant("Joe", 40)
+  end
+
+  def test_total_cost
+    # skip
+    activity = Activity.new("Brunch")
+    expected = 20
+
+    assert_equal expected, activity.total_cost("Jim", 20)
+  end
+
+  def test_it_adds_participant_in_total_cost
+    activity = Activity.new("Brunch")
+    expected =
+    activity.add_participant("Jim", 20)
+    activity.add_participant("Joe", 40)
+
+    assert_equal expected,
+  end
+
+
 end
